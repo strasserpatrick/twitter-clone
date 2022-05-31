b11d48f00e6bd7da716177e709f16095a97045f6
s368923 <patrick.strasser@stud-mail.uni-wuerzburg.de>
2022-05-31 18:55:42 +0200

    added post endpoints and tests

--- test/test_api_post.py
+++ test/test_api_post.py
@@ -3,11 +3,10 @@ from pymongo.errors import DuplicateKeyError
 
 from conftest import url
 from owntwitter.models.exceptions import UserNotFoundException
-from owntwitter.models.factories import UserFactory
+from owntwitter.models.factories import UserFactory, PostFactory
 
 
 def test_create_user(client, db_service_dependency_override):
-
     user = UserFactory.build()
     user_json = jsonable_encoder(user)
 
@@ -24,11 +23,31 @@ def test_create_user_exists(client, db_service_dependency_override):
     r = client.post(url + f"/create/user", json=user_json)
     assert r.status_code == 404
 
+
 def test_create_post(client, db_service_dependency_override):
-    pass
+
+    post = PostFactory.build()
+    post_json = jsonable_encoder(post)
+
+    r = client.post(url + f"/create/post", json=post_json)
+    assert r.status_code == 201
+
 
 def test_create_post_duplicate(client, db_service_dependency_override):
-    pass
+    db_service_dependency_override.create_new_post.side_effect = DuplicateKeyError("error")
+
+    post = PostFactory.build()
+    post_json = jsonable_encoder(post)
+
+    r = client.post(url + f"/create/post", json=post_json)
+    assert r.status_code == 404
+
 
 def test_create_post_user_not_found(client, db_service_dependency_override):
-    pass
+    db_service_dependency_override.create_new_post.side_effect = UserNotFoundException
+
+    post = PostFactory.build()
+    post_json = jsonable_encoder(post)
+
+    r = client.post(url + f"/create/post", json=post_json)
+    assert r.status_code == 404
